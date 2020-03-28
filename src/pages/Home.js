ecfc44ca5ef4baa38a74649505dcdd00a2c77b90
temovargas <48292804+temovargas@users.noreply.github.com>
2020-03-27 22:40:20 -0400

    check if user owens the company
    check if user owens the company, Either  adds the whole company or updates just the data if the user owens it.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import currency from 'currency.js'
 import axios from 'axios'
 import { getHoldingsData } from '../utils/herlper'
 import FormContainer from '../compoents/FormContainer'
@@ -26,7 +27,7 @@ export default class Home extends Component {
         {
           ticker: 'T',
           shares: '4',
-          avgPricePaid: '145',
+          avgPricePaid: '30',
           lastDividend: '',
           frequency: '',
           yearlyDividend: ''
@@ -73,22 +74,52 @@ export default class Home extends Component {
 
   handleAddHolding = evt => {
     evt.preventDefault()
-    const { tickerInput, sharesInput, avgPricePaidInput } = this.state
+    const { tickerInput, sharesInput, avgPricePaidInput, holdings } = this.state
+    // get all companines the user ownes
+    holdings.map((company, id) => {
+      if (company.ticker === tickerInput) {
+        const arrayItiemId = id
 
-    const newHolding = {
-      ticker: tickerInput,
-      shares: sharesInput,
-      avgPricePaid: avgPricePaidInput
-    }
-    // reset inputs,
-    // hide modal
-    // add holdings
-    this.setState({
-      showModal: false,
-      tickerInput: '',
-      sharesInput: '',
-      avgPricePaidInput: '',
-      holdings: [...this.state.holdings, newHolding]
+        // add all the shares
+        const updatedShares = currency(company.shares)
+          .add(sharesInput)
+          .format()
+        // add all the shares
+        // MIGHT REMOVE
+        const updatedAvgPricePaid = currency(company.avgPricePaid)
+          .add(avgPricePaidInput)
+          .format()
+
+        const updatedArray = {
+          ...company,
+          shares: updatedShares,
+          avgPricePaid: updatedAvgPricePaid
+        }
+        // remove current company from array
+        holdings.splice(arrayItiemId, 1)
+        // push updated company array
+        holdings.push(updatedArray)
+
+        this.setState({
+          holdings: holdings
+        })
+      } else {
+        const newHolding = {
+          ticker: tickerInput,
+          shares: sharesInput,
+          avgPricePaid: avgPricePaidInput
+        }
+        // reset inputs,
+        // hide modal
+        // add holdings
+        this.setState({
+          showModal: false,
+          tickerInput: '',
+          sharesInput: '',
+          avgPricePaidInput: '',
+          holdings: [...this.state.holdings, newHolding]
+        })
+      }
     })
   }
 
