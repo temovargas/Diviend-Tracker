0e45df0dc0d0607d2af344df7f09476cc2edd481
temovargas <48292804+temovargas@users.noreply.github.com>
2020-04-14 00:07:48 -0400

    Update Nav.js

--- src/compoents/Nav.js
+++ src/compoents/Nav.js
@@ -3,15 +3,22 @@ import { NavLink } from 'react-router-dom'
 
 const Nav = () => (
   <nav>
-    <li>
-      <NavLink to="/">Home</NavLink>
-    </li>
-    <li>
-      <NavLink to="/stocksearch">Search Sotck</NavLink>
-    </li>
-    <li>
-      <NavLink to="/about">About</NavLink>
-    </li>
+    <div className="wrapper nav">
+      <li className="heading">
+        <NavLink to="/">
+          <h1>Dividend</h1>
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/stocksearch">Search Sotck</NavLink>
+      </li>
+      <li>
+        <NavLink to="/about">About</NavLink>
+      </li>
+    </div>
   </nav>
 )
 
