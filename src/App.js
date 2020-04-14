4f6915b2b84b0268b0ef285fef1935266e555cc2
temovargas <48292804+temovargas@users.noreply.github.com>
2020-04-14 00:06:50 -0400

    removed teset button and moved nav

--- src/App.js
+++ src/App.js
@@ -1,30 +1,17 @@
 import React from 'react'
-import axios from 'axios'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 // Compoents
 import Nav from './compoents/Nav'
 import StockSearch from './pages/SockSearch'
 import About from './pages/About'
 import Home from './pages/Home'
-function testbtn() {
-  axios
-    .get(
-      `https://cloud.iexapis.com/stable/data-points/T/DIVIDENDYIELD?token=${process.env.REACT_APP_IXE_API_KEY}`
-    )
-    .then(data => {
-      console.log('====================================')
-      console.log(data.data)
-      console.log('====================================')
-    })
-}
+
 function App() {
   return (
     <div className="App">
       <Router>
-        <Nav />
         <header className="App-header">
-          <h1>Dividend Porfolio</h1>
-          <button onClick={testbtn}>Click me</button>
+          <Nav />
         </header>
         {/* Routes */}
         <Switch>
