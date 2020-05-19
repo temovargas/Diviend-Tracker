44d58603259ead2b5e6a1369e42ffd48c09c5059
temovargas <48292804+temovargas@users.noreply.github.com>
2020-05-19 13:09:55 -0400

    removed css styles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import './master.css'
+import './normalize.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
