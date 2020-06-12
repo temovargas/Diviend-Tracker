bf9c40b3d01b7b92bf768e2785f5c7971806a72a
temovargas <48292804+temovargas@users.noreply.github.com>
2020-06-12 14:28:54 -0400

    moved input to input folder

--- src/compoents/Input/Input.js
+++ src/compoents/Input/Input.js
@@ -1,18 +1,20 @@
 import React from 'react'
+import './Input.css'
 
 const Input = props => {
+  console.log(props)
   return (
     <div className="form-group">
       <label htmlFor={props.name} className="form-label">
         {props.title}
       </label>
       <input
-        className="form-input"
+        className={`form-input ${props.className}`}
         id={props.name}
         name={props.name}
         type={props.type}
-        value={props.value}
         onChange={props.handleChange}
+        value={props.value}
         placeholder={props.placeholder}
       />
     </div>
