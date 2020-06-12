dcb63e5350a60fffdc5b25824e49ec7493f73fe5
temovargas <48292804+temovargas@users.noreply.github.com>
2020-06-12 14:27:47 -0400

    added basic styles

--- src/compoents/FormContainer.js
+++ src/compoents/FormContainer.js
@@ -1,5 +1,9 @@
 import React from 'react'
 
 export default function FormContainer(props) {
-  return <form className={props.className}>{props.children}</form>
+  return (
+    <form onSubmit={props.onSubmit} className={props.className}>
+      {props.children}
+    </form>
+  )
 }
