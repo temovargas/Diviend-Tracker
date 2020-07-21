b13f8bdab61ce376bffa23e3f3e3979a1bd538d7
temovargas <48292804+temovargas@users.noreply.github.com>
2020-07-21 13:07:52 -0400

    moved FormContainer to its own folder and added css file

--- src/compoents/FormContainer/FormContainer.js
+++ src/compoents/FormContainer/FormContainer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import './FormContainer.css'
 export default function FormContainer(props) {
   return (
     <form onSubmit={props.onSubmit} className={props.className}>
